bdd82227e7809a49c602157edcea3f977d773338
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-03-20 07:32:03 -0300

    feat: generateStaticParams

--- src/app/e-commerce/products/[id]/page.tsx
+++ src/app/e-commerce/products/[id]/page.tsx
@@ -1,5 +1,25 @@
 import { IProduct } from "../../types/products"
 
+
+export async function generateStaticParams() {
+  const products: IProduct[] = await fetch(`https://api.origamid.online/produtos`, {
+    cache: 'no-store'
+    // next: {
+    //   revalidate: 5,
+    //   tags: ['products']
+    // }
+  })
+    .then((response) => {
+      // if (!response.ok) throw new Error('Erro ao buscar produtos')
+      return response.json()
+    }).catch((error) => {
+      console.error(error.message)
+      throw new Error('Erro ao buscar produto')
+    })
+
+  return products?.map((product) => ({ id: String(product.id) }))
+}
+
 export default async function ProductPage({ params }: { params: { id: string } }) {
   const id = params?.id
 
